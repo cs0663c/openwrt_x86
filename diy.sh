1f82a1eae4a1a233a5dd19b3d13e0a47ec5341f8
cs0663c <59052041+cs0663c@users.noreply.github.com>
2020-04-23 22:09:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,5 @@
 sed -i 's/192.168.1.1/192.168.3.250/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+./scripts/feeds update -a
+./scripts/feeds install -a
